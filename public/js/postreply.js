f0431af9cabba73bc176738fee959d0808fd92c8
MicroFish91 <mwfisher91@gmail.com>
2021-06-18 12:11:13 -0700

    scaffold postreply

--- public/js/postreply.js
+++ public/js/postreply.js
@@ -1,27 +1,18 @@
-let submitForm = document.querySelector('#contactForm')
+let submitButton = document.query ...
 
-        submitForm.addEventListener('submit',(e)=>{
-          e.preventDefault();
-          let name= document.querySelector('#name').value
-          let email= document.querySelector('#email').value
-          let message= document.querySelector('#message').value
-          let subject= document.querySelector('#subject').value
-          let data = { name, email, message, subject };
+submitButton.addEventListener('click',(e)=>{
+  e.preventDefault();
+  let reply = document.query ....
+  let data = { comment: reply.value };
 
-          fetch("https://submit-form.com/kEipJxSO",{
-              method:"POST",
-              headers:{
-                    'Content-type': 'application/json; charset=UTF-8' // Indicates the content 
-                },
-              body:JSON.stringify(data)
-          })
-          .then(data=>console.log('success'))
-          .catch(err=>console.log('err'));
+  fetch(window.location.href,{
+      method:"POST",
+      headers:{
+            'Content-type': 'application/json; charset=UTF-8' // Indicates the content 
+        },
+      body:JSON.stringify(data)
+  })
+  .then(data=>console.log('success'))
+  .catch(err=>console.log('err'));
 
-          document.querySelector('#name').value = "";
-          document.querySelector('#email').value = "";
-          document.querySelector('#message').value = "";
-          document.querySelector('#subject').value = "";
-
-          alert("Message sent!");
-        });       
+});       
